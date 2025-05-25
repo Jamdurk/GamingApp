f0a6f73bb65e8ea901150f281cfb69766fde1869
James <james.savona@outlook.com>
2025-05-25 14:30:29 +1000

    Added home page nav bar test to pages controller

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -8,6 +8,12 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "home page displays title and navigation" do
+    get root_path
+    assert_select "h1", "Gaming App 2025"
+    assert_select "nav a", count: 3  # Home, Recordings, Submission
+  end
+
 end
 
   
