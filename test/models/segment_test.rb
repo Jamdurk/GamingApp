67f3a2193adc1a40a05f8bfd697b476ac3d2d88f
James <james.savona@outlook.com>
2025-05-25 14:26:10 +1000

    Segment model testing suite complete

--- test/models/segment_test.rb
+++ test/models/segment_test.rb
@@ -1,7 +1,80 @@
 require "test_helper"
 
 class SegmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is invalid without transcript" do
+    segment = Segment.new
+    assert_not segment.valid?
+    assert_includes segment.errors[:transcript], "must exist"
+  end
+
+  test "is valid with transcript" do
+    transcript = transcripts(:one)
+    segment = Segment.new(
+      transcript: transcript,
+      start_time: 0,
+      end_time: 10,
+      text: "Hello, world!"
+    )
+    assert segment.valid?
+  end
+  
+  test "is invalid without start_time" do
+    transcript = transcripts(:one)
+    segment = Segment.new(
+      transcript: transcript,
+      start_time: nil,
+      end_time: 10,
+      text: "Hello, world!"
+    )
+    assert_not segment.valid?
+    assert_includes segment.errors[:start_time], "can't be blank"
+  end
+
+  test "is invalid without end_time" do
+    transcript = transcripts(:one)
+    segment = Segment.new(
+      transcript: transcript,
+      start_time: 0,
+      end_time: nil,
+      text: "Hello, world!"
+    )
+    assert_not segment.valid?
+    assert_includes segment.errors[:end_time], "can't be blank"
+  end
+
+  test "is invalid without text" do
+    transcript = transcripts(:one)
+    segment = Segment.new(
+      transcript: transcript, 
+      start_time: 0,
+      end_time: 10,
+      text: nil
+    )
+    assert_not segment.valid?
+    assert_includes segment.errors[:text], "can't be blank"
+  end
+
+  test "is invalid when start_time is greater than end_time" do
+    transcript = transcripts(:one)
+    segment = Segment.new(
+      transcript: transcript, 
+      start_time: 5,
+      end_time: 1,
+      text: "Hello, world!"
+    )
+    assert_not segment.valid?    
+    assert_includes segment.errors[:start_time], "must not be after end time"
+  end
+  
+  test "is valid when start_time is equal to but not greater than end_time" do
+    transcript = transcripts(:one)
+    segment = Segment.new(
+      transcript: transcript,
+      start_time: 10,
+      end_time: 10,
+      text: "Hello, world!"
+    )
+    assert segment.valid?
+  end
+  
 end
