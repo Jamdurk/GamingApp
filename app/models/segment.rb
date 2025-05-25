d79cbd5afa149b280aa840411457d8fa8497a699
James <james.savona@outlook.com>
2025-05-25 14:22:41 +1000

    Added start_time cannot be greater than end_time validation

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -3,4 +3,13 @@ class Segment < ApplicationRecord
 
 
   validates :start_time, :end_time, :text, presence: true 
+  validate :start_must_be_before_end
+
+  private
+
+  def start_must_be_before_end
+    if start_time.present? && end_time.present? && start_time > end_time
+      errors.add(:start_time, "must not be after end time")
+    end
+  end
 end
