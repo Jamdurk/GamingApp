c16bef76cb5cd233c789c1f8556f80d4525c5851
James <james.savona@outlook.com>
2025-05-26 17:29:11 +1000

    Added variables to controller for home page to display stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @total_recordings = Recording.count
+    @total_clips      = Clip.count
+    @total_hours      = Recording.all.sum do |recording|
+      if recording.video.attached?
+        metadata = recording.video.blob.metadata
+        seconds  = metadata.dig("custom", "asv_duration")&.to_f
+        seconds.to_f / 3600
+      else
+        0
+      end
+    end
   end
+  
 end
