b5d215989ff63e7a2df43ce87f91a71d8c243e99
James <james.savona@outlook.com>
2025-05-27 13:38:48 +1000

    Added create/new & clip param actions to controller

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,7 +1,36 @@
 class ClipsController < ApplicationController
   def new
+    @recording = Recording.find(params[:recording_id])
+    @clip      = @recording.clips.build
   end
 
   def create
+    @recording = Recording.find(params[:recording_id])
+    @clip      = @recording.clips.build(clips_params)
+
+    if @clip.save 
+      result = ClipGenerationService.call(
+        recording: @recording,
+        start_time: @clip.start_time,
+        end_time: @clip.end_time,
+        title: @clip.title
+      )
+
+      if result.success?
+        redirect_to @recording, notice: 'Clip created successfully!'
+      else
+        @clip.errors.add(:base, result.error)
+        render :new
+      end
+    else
+      render :new
   end
+end
+
+private 
+
+def clips_params
+  params.require(:clip).permit(:title, :start_time, :end_time)
+end
+
 end
--- test/controllers/clips_controller_test.rb
+++ test/controllers/clips_controller_test.rb
@@ -1,13 +1,5 @@
 require "test_helper"
 
 class ClipsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get clips_new_url
-    assert_response :success
-  end
 
-  test "should get create" do
-    get clips_create_url
-    assert_response :success
-  end
 end
