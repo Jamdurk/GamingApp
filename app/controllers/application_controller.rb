0c0a80717c7f41384f8a738f69f23ea619dcee4e
James <james.savona@outlook.com>
2025-05-31 12:29:23 +1000

    find recording method had an issue in the find line, params were structured wrong causing tests to fail, however has been fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,9 @@ class ApplicationController < ActionController::Base
 
 # Basically if someone is dumb and tries to put the url directly with some random recording ID that doesnnt exist this redirects and handles it rather than a 404
 def find_recording
-  @recording = Recording.find(params[:recording_id])
+  @recording = Recording.find(params[:recording_id] || params[:id])
 rescue ActiveRecord::RecordNotFound
   redirect_to recordings_path, alert: "Recording not found"
 end
-
 
 end
