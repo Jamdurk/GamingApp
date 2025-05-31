07dbc0698b2b9c9d57d1e4c1bd3660e9a998eb83
James <james.savona@outlook.com>
2025-05-31 23:00:23 +1000

    Same as previous strucuturing queues for sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+
+require 'sidekiq-limit_fetch'
+
+
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    Sidekiq::Queue['transcription'].limit = 1
+    Sidekiq::Queue['subtitles'].limit = 1
+  end
+end
