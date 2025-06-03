5c3ef3808a71fe5abd1c1d8d35ace2fbb44bdb42
James <james.savona@outlook.com>
2025-06-03 14:56:32 +1000

    Added support and queue structure for clip job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,5 +6,6 @@ Sidekiq.configure_server do |config|
   config.on(:startup) do
     Sidekiq::Queue['transcription'].limit = 1
     Sidekiq::Queue['subtitles'].limit = 1
+    Sidekiq::Queue['clips'].limit = 1
   end
 end
