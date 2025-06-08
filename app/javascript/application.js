dcc2973a13f78526bf2b90954912e8f9d2ac2761
James <james.savona@outlook.com>
2025-06-08 14:58:03 +1000

    Various changes as javascript was being blocked on production

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "./starfield"
+import "starfield"
 import "controllers"
 import "video.js"  // Just import it, don't assign to a variable
 
