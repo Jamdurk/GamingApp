21f2c068d9e712ad3877ef6c1eb466465ed6d38c
James <james.savona@outlook.com>
2025-06-27 12:18:53 +1000

    Added support for both environments, both dev and prod as prior was only productionm support which broke uploads in dev

--- app/services/video_utils.rb
+++ app/services/video_utils.rb
@@ -1,6 +1,11 @@
 module VideoUtils
   def download_video
-    output_dir = "/video_storage" # Use the mounted 40GB volume
+    output_dir = if Rails.env.production?
+                   "/video_storage"  # Existing production path (volume 40gb attached to sidekiq machine on fly.io)
+                 else
+                   Rails.root.join("tmp", "video_storage").to_s  # Dev env path for uploading locally
+                 end
+    
     FileUtils.mkdir_p(output_dir)
   
     temp_path = File.join(output_dir, "original_#{@recording.id}_#{Time.now.to_i}.mp4")
@@ -9,10 +14,10 @@ module VideoUtils
       FileUtils.cp(file.path, temp_path)
       FileUtils.chmod(0644, temp_path)
     end
-  
-    temp_path
-  end
-  
+
+    temp_path  # Return the temp_path
+  end  # This end was missing!
+
   def deduplicate_segments(segments, max_repetitions=3) # Was originally using this in my transcription service, but removed due to some conflicts. Storing here in case for future use.
     result = []
     current_text = nil
